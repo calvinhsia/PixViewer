92a5630de1b30f3e535ce56b6159a03b755c7bcf
Calvin Hsia <calvinh@microsoft.com>
2020-09-07 15:56:06 -0700

    Add project files.

---
+++ PixViewer/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PixViewer
+{
+    /*
+<Window x:Class="PixViewer.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:PixViewer"
+        mc:Ignorable="d"
+        Title="MainWindow" Height="450" Width="1200">
+    <Grid>
+        <Grid.RowDefinitions>
+            <RowDefinition Height="*"/>
+            <RowDefinition Height="40"/>
+        </Grid.RowDefinitions>
+        <DockPanel Grid.Row="0" Name="dp"/>
+        <StackPanel Grid.Row="1" Orientation="Horizontal">
+            <TextBox Text="{Binding CurPicIndex }" Width="50"/>
+            <TextBox Text="{Binding Filename}" Width="300"/>
+            <TextBox Text="{Binding Dtime}" Width="150"/>
+            <Label Content="{Binding Notes}" Width="700"/>
+            <Button Name="btngoBack" Content="&lt;" Width="20" Click="btngo_Click"/>
+            <Button Name="btngoForward" Content=">" Width="20" Click="btngo_Click" />
+        </StackPanel>
+    </Grid>
+</Window>
+
+
+Watch out for embedded quote marks in notes.
+BROWSE FOR ATC("lila",notes)>0
+COPY TO t.txt csv
+
+     */
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public string Filename { get; set; }
+        public DateTime Dtime { get; set; }
+        public string Notes { get; set; }
+        public string CurPicIndex { get; set; }
+
+        string rootPix = @"c:\users\calvinh\OneDrive\Pictures\OldPictures\";
+        string targFolder = @"C:\Users\calvinh\OneDrive\QueryResults";
+        public event PropertyChangedEventHandler PropertyChanged;
+        void RaisePropChanged([CallerMemberName] string propName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        List<PicData> lstAllPicData = new List<PicData>();
+
+        List<PicData> lstFilteredPicData = new List<PicData>();
+        int curNdx;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            this.DataContext = this;
+            this.Loaded += (o, e) =>
+            {
+                this.cboQueryString.Initialize(nameof(Properties.Settings.Default.QueryString));
+                var fnCSV = Environment.ExpandEnvironmentVariables(@"c:\pictures\t.txt");
+                var firstline = true;
+                var dictFields = new Dictionary<string, int>(); //fldname->index
+                using (var parser = new Microsoft.VisualBasic.FileIO.TextFieldParser(fnCSV))// handles quoted values in csv
+                {
+                    parser.Delimiters = new[] { "," };
+                    while (!parser.EndOfData)
+                    {
+                        var flds = parser.ReadFields();
+                        if (firstline)
+                        {
+                            foreach (var item in flds)
+                            {
+                                dictFields[item] = dictFields.Count;
+                            }
+                            firstline = false;
+                        }
+                        else
+                        {
+                            var data = new PicData()
+                            {
+                                fullname = flds[dictFields["fullname"]],
+                                dtime = DateTime.Parse(flds[dictFields["dtime"]]),
+                                notes = flds[dictFields["notes"]],
+                                rotate = int.Parse(flds[dictFields["rotate"]])
+                            };
+                            //                              if (System.IO.Path.GetExtension(data.fullname).ToLower() == ".jpg")
+                            {
+                                lstAllPicData.Add(data);
+                                //var targFilename = System.IO.Path.Combine(targFolder, System.IO.Path.GetFileName(data.fullname));
+                                //if (!File.Exists(targFilename))
+                                //{
+                                //    var picFile = System.IO.Path.Combine(rootPix, data.fullname);
+                                //    File.Copy(picFile, targFilename);
+                                //}
+                            }
+                        }
+                    }
+                }
+                this.curNdx = -1;
+                this.btnQuery.RaiseEvent(new RoutedEventArgs() { RoutedEvent = Button.ClickEvent, Source = this });
+            };
+        }
+        class PicData
+        {
+            public string fullname;
+            public DateTime dtime;
+            public string notes;
+            public int rotate;
+        }
+        void ShowPic(PicData fileData)
+        {
+            var picFile = System.IO.Path.Combine(rootPix, fileData.fullname);
+            if (File.Exists(picFile))
+            {
+                var im = new BitmapImage(new Uri(picFile));
+                this.dp.Children.Clear();
+                this.dp.Children.Add(new Image() { Source = im });
+                this.CurPicIndex = $"{curNdx}/{lstFilteredPicData.Count}";
+                this.Filename = fileData.fullname;
+                this.Dtime = fileData.dtime;
+                this.Notes = fileData.notes;
+                RaisePropChanged(nameof(CurPicIndex));
+                RaisePropChanged(nameof(Dtime));
+                RaisePropChanged(nameof(Filename));
+                RaisePropChanged(nameof(Notes));
+            }
+        }
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            base.OnPreviewKeyDown(e);
+            switch (e.Key)
+            {
+                case Key.Left:
+                    this.btnNavBack.RaiseEvent(new RoutedEventArgs() { RoutedEvent = Button.ClickEvent, Source = this.btnNavBack });
+                    e.Handled = true;
+                    break;
+                case Key.Right:
+                    this.btnNavForward.RaiseEvent(new RoutedEventArgs() { RoutedEvent = Button.ClickEvent, Source = this });
+                    e.Handled = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btnNav_Click(object sender, RoutedEventArgs e)
+        {
+            var incr = 1;
+            if (sender is Button btn && btn.Name == nameof(this.btnNavBack))
+            {
+                incr = -1;
+            }
+            if (this.curNdx + incr >= 0 && this.curNdx + incr < this.lstFilteredPicData.Count)
+            {
+                this.curNdx += incr;
+                ShowPic(this.lstFilteredPicData[this.curNdx]);
+            }
+        }
+
+        private void btnQuery_Click(object sender, RoutedEventArgs e)
+        {
+            lstFilteredPicData = lstAllPicData.Where(t => t.notes.IndexOf(cboQueryString.Text) >= 0).ToList();
+            curNdx = -1;
+            btnNavForward.RaiseEvent(new RoutedEventArgs() { RoutedEvent = Button.ClickEvent, Source = this });
+        }
+    }
+}
---
+++ PixViewer/MyComboBox.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PixViewer
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:MemWatsonKusto"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:MemWatsonKusto;assembly=MemWatsonKusto"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:MyComboBox/>
+    ///
+    /// </summary>
+    public class MyComboBox : ComboBox
+    {
+        private PropertyInfo typSettings;
+
+        //static MyComboBox()
+        //{   
+        //    //https://stackoverflow.com/questions/6695888/wpf-custom-controls-are-invisible
+        //    //[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly))]
+        //    //DefaultStyleKeyProperty.OverrideMetadata(typeof(MyComboBox), new FrameworkPropertyMetadata(typeof(MyComboBox)));
+        //}
+        public void Initialize(string ValueSettingName)
+        {
+            typSettings = Properties.Settings.Default.GetType().GetProperty(ValueSettingName);
+            var initValues = (StringCollection)typSettings.GetValue(Properties.Settings.Default);
+            this.Items.Clear();
+            if (initValues?.Count > 0)
+            {
+                foreach (var itm in initValues)
+                {
+                    this.Items.Add(new ComboBoxItem() { Content = itm });
+                }
+                this.Text = initValues[0];
+            }
+            this.SelectedIndex = 0;
+        }
+        /// <summary>
+        /// If the text is not in the list, then add it at beginning of the list and return true. Persist settings
+        /// </summary>
+        public bool CommitData()
+        {
+            var isNewItem = true;
+            foreach (ComboBoxItem itm in this.Items)
+            {
+                if ((string)itm.Content == this.Text)
+                {
+                    isNewItem = false;
+                    break;
+                }
+            }
+            if (isNewItem)
+            {
+                this.Items.Insert(0, new ComboBoxItem() { Content = this.Text });
+                var col = new StringCollection();
+                foreach (ComboBoxItem itm in this.Items)
+                {
+                    col.Add((string)itm.Content);
+                }
+                typSettings.SetValue(Properties.Settings.Default, col);
+                Properties.Settings.Default.Save();
+            }
+            return isNewItem;
+        }
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            base.OnPreviewKeyDown(e);
+            if (this.IsDropDownOpen)
+            {
+                if (e.Key == Key.Delete)
+                {
+                    if (Items.Count > 0 && this.SelectedIndex >= 0)
+                    {
+                        Items.RemoveAt(SelectedIndex);
+                        if (Items.Count > 0)
+                        {
+                            SelectedIndex = 0;
+                        }
+                        e.Handled = true;
+                    }
+                }
+            }
+        }
+    }
+}
